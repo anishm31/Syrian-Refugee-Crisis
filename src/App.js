c271cee1e1a3ffe034c74d6a65e84ab37f9ec312
Anish Madgula <animadgula@utexas.edu>
2023-09-21 19:48:20 -0500

    Basic home page, navbar, router packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import MainNavbar from './MainNavbar.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MainNavbar from './Components/MainNavbar.js';
 
 function App() {
   return (
