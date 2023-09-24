7a0af7e0c5ea8548d253de32b567146f23b17e0b
Matthew Albert <matthew.albert@utexas.edu>
2023-09-24 14:33:19 -0500

    fixed instance grid sizing problem

--- src/Components/InstanceGrid.js
+++ src/Components/InstanceGrid.js
@@ -10,7 +10,7 @@ function InstanceGrid(props) {
       <div className="title-holder mb-3">
         <h3>{instanceList.length} Instances</h3>
       </div>
-      <Row className='d-flex justify-content-center'>
+      <Row className='d-flex justify-content-center' xs={2} md={3} lg={3} xlg={4}>
         {instanceList.map((instance) => {
           return (
             <Col className="mb-3" key={instance.id}>
