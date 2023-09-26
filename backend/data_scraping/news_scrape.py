5e2a138eb0e07fdc1b70e6028791a753700035f0
Shadow Sincross <shadowanneruby@gmail.com>
2023-09-26 16:26:22 -0500

    scraped more data for news card

--- backend/data_scraping/news_scrape.py
+++ backend/data_scraping/news_scrape.py
@@ -18,7 +18,7 @@ json_file_path = "./models_data/news_db.json"
 # Iterate through each endpoint to scrape necessary attributes
 #got rid of params
     # Make request to UNHCR API
-response = requests.get(f"https://api.reliefweb.int/v1/reports?appname=apidoc&query[value]=syria&query[fields][]=source&fields[include][]=source.name")
+response = requests.get(f"https://api.reliefweb.int/v1/reports?appname=apidoc&query[value]=Syrian Refugee&query[fields][]=source&fields[include][]=source.shortname&fields[include][]=disaster_type.name&fields[include][]=primary_country.shortname&fields[include][]=date.created&fields[include][]=image&fields[include][]=headline.title")
 if response.status_code == 200:
       # Success, store data in json template
       response_data = response.json()
@@ -27,15 +27,33 @@ if response.status_code == 200:
       item_index = -1
 
 
+
       #loop through all of the reports on syrian refugees 
       for report in response_data["data"]:
+        instance_template = {
+        "Title" : "", "Date": "", "Source":"", "Image":"",
+        "attributes" : {
+        "Disaster type" : "",
+        "Primary Country" : "",
+        "Other Sources": {},
+    }
+        }
         # Declare template for storing data in json format
         fields = report["fields"]
         title = fields.get("title")
-        instance_template = {
-        "title" : "",}
+        date = fields.get("date").get("created")
+        primary_country = fields.get("primary_country").get("shortname")
+        image = fields.get("headline")
+
+        if image is not None:
+          image = fields.get("headline").get("image")
+          if image is not None:
+            image = fields.get("headline").get("image").get("url")
   
-        instance_template["title"]=title
+        instance_template["Title"]=title
+        instance_template["Date"]=date
+        instance_template["attributes"]["Primary Country"]=primary_country
+    
         news_data.append(instance_template)
 else:
       # Error, print status code
