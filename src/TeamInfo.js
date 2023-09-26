359400f7e18418f8b2557baa6924900929d68720
Anish Madgula <animadgula@utexas.edu>
2023-09-26 17:49:42 -0500

    closes #15, scraped Gitlab API and created basic team member cards with the relevant info

---
+++ src/TeamInfo.js
@@ -0,0 +1,57 @@
+// import Petar from "./images/petar.jpg";
+// import Tanya from "./images/tanya.png";
+// import Audrey from "./images/audrey.jpg";
+// import Noah from "./images/noah.jpg";
+// import Pawan from "./images/pawan.png";
+import Anish from "./images/anish.png";
+import { Placeholder } from "react-bootstrap";
+
+const TeamInfo = [
+  {
+    image: Anish,
+    name: "Anish Madgula",
+    user: "animadgula",
+    user_id: "15725067",
+    role: "Developer",
+    commits: 0,
+    issues: 0,
+  },
+  {
+    image: Placeholder,
+    name: "Anish Madgula",
+    user: "animadgula",
+    user_id: "15725067",
+    role: "Developer",
+    commits: 0,
+    issues: 0,
+  },
+  {
+    image: Placeholder,
+    name: "Anish Madgula",
+    user: "animadgula",
+    user_id: "15725067",
+    role: "Developer",
+    commits: 0,
+    issues: 0,
+  },
+  {
+    image: Placeholder,
+    name: "Anish Madgula",
+    user: "animadgula",
+    user_id: "15725067",
+    role: "Developer",
+    commits: 0,
+    issues: 0,
+  },
+  {
+    image: Placeholder,
+    name: "Anish Madgula",
+    user: "animadgula",
+    user_id: "15725067",
+    role: "Developer",
+    commits: 0,
+    issues: 0,
+  },
+];
+
+export { TeamInfo };
