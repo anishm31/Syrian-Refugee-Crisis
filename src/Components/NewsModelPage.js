a0d8b7c355db0512835e3a98a7d57219b75aa5da
Shadow Sincross <shadowanneruby@gmail.com>
2023-09-26 03:30:10 -0500

    fixed news/events model page

--- src/Components/NewsModelPage.js
+++ src/Components/NewsModelPage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import GenericModelPage from "./GenericModelPage";
 import NewsCard from "./NewsCard.js";
-import news_data from "../model_data/news_db.json";
+import newsData from "../model_data/news_db.json";
+
 
 function NewsModelPage() {
   return (
     <GenericModelPage
       model="News"
       modelCard={NewsCard}
-      instances={news_data}
+      instances={newsData}
     />
   );
 }
