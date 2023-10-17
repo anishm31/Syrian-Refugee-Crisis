3dc1d772fbf57ff9326696030a0529beb43d3850
Anish Madgula <animadgula@utexas.edu>
2023-10-17 18:17:53 -0500

    fix pipeline error

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|svg)$": "identity-obj-proxy",
-      "\\.(css|less|scss)$": "identity-obj-proxy",
-    },
-    testEnvironment: 'jsdom',
-  };
