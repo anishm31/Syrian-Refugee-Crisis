1f3ac22e37cf2013bc690d8a85dfb9ca290a86cd
Anish Madgula <animadgula@utexas.edu>
2023-10-17 17:49:52 -0500

    closes #47, created 10 Jest unit tests

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -6,35 +6,35 @@ import { Link } from 'react-router-dom';
 
 
 function NewsCard(props) {
-return (
-<Card style={{ width: "90%" }}>
-<Card.Img
-variant="top"
-src={props.instance.Image}
-style={{ objectFit : "cover", width: "100%", height: "200px"}}
-/>
-<Card.Body style={{ textAlign: "left" }}>
-<Card.Title>{props.instance.title}</Card.Title>
-<Card.Text style={{ fontSize: "small" }}>Name of Event: {props.instance.Title}</Card.Text>
-<Card.Text>Date of Event {props.instance.Date.toLocaleString()}</Card.Text>
-</Card.Body>
-<ListGroup className="list-group-flush" style={{ textAlign: "left" }}>
-<ListGroup.Item>Location of Event: {props.instance.attributes.PrimaryCountry.toLocaleString()}</ListGroup.Item>
-<ListGroup.Item>Disaster Type: {props.instance.attributes.DisasterType.toLocaleString()}</ListGroup.Item>
-<ListGroup.Item>Source: {props.instance.attributes.OtherSources.toLocaleString()}</ListGroup.Item>
-<ListGroup.Item>Theme: {props.instance.attributes.Themes.toLocaleString()}</ListGroup.Item>
-</ListGroup>
-<Card.Body>
-<Button variant="outline-success">
-<Link
-style={{ color: "black", textDecoration: "inherit"}}
-to={"/news-and-events/" + props.instance.Title}>
-More Info
-</Link>
-</Button>
-</Card.Body>
-</Card>
-);
+    return (
+        <Card style={{ width: "90%" }}>
+            <Card.Img
+                variant="top"
+                src={props.instance.Image}
+                style={{ objectFit : "cover", width: "100%", height: "200px"}}
+            />
+            <Card.Body style={{ textAlign: "left" }}>
+                <Card.Title>{props.instance.title}</Card.Title>
+                <Card.Text style={{ fontSize: "small" }}>Name of Event: {props.instance.Title}</Card.Text>
+                <Card.Text>Date of Event {props.instance.Date.toLocaleString()}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush" style={{ textAlign: "left" }}>
+                <ListGroup.Item>Location of Event: {props.instance.attributes.PrimaryCountry.toLocaleString()}</ListGroup.Item>
+                <ListGroup.Item>Disaster Type: {props.instance.attributes.DisasterType.toLocaleString()}</ListGroup.Item>
+                <ListGroup.Item>Source: {props.instance.attributes.OtherSources.toLocaleString()}</ListGroup.Item>
+                <ListGroup.Item>Theme: {props.instance.attributes.Themes.toLocaleString()}</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+    <           Button variant="outline-success">
+                    <Link
+                        style={{ color: "black", textDecoration: "inherit"}}
+                        to={"/news-and-events/" + props.instance.Title}>
+                        More Info
+                    </Link>
+                </Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 
--- src/tests/Frontend.test.js
+++ src/tests/Frontend.test.js
@@ -3,6 +3,16 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SearchBar from "../Components/SearchBar";
 import InstanceGrid from "../Components/InstanceGrid";
+import TeamCard from "../Components/TeamCard";
+import GenericModelPage from "../Components/GenericModelPage";
+import CharityModelPage from "../Components/CharityModelPage";
+import CharityCard from "../Components/CharityCard";
+import NewsEventsModelPage from "../Components/NewsEventsModelPage";
+import NewsCard from "../Components/NewsCard";
+import CountryModelPage from "../Components/CountryModelPage";
+import CountryCard from "../Components/CountryCard";
+import HomePage from "../Components/HomePage";
+import HomeCarousel from "../Components/HomeCarousel";
 
 describe('SearchBar Component', () => {
   it('should render the search form textbox element', () => {
@@ -34,5 +44,126 @@ describe('SearchBar Component', () => {
 
     expect(inputPlaceholder).toBeInTheDocument();
     expect(searchButton).toBeInTheDocument();
+  });
+});
+
+describe('TeamCard Component', () => {
+  it('should render the team card with the correct information', () => {
+    const sampleData = {
+      name: 'John Doe',
+      image: 'john-doe-image.jpg',
+      user: 'johndoe123',
+      role: 'Developer',
+      bio: 'Passionate about web development',
+      commits: 100,
+      issues: 50,
+    };
+
+    render(<TeamCard {...sampleData} />);
+
+    const nameElement = screen.getByText('John Doe');
+    const userElement = screen.getByText('@johndoe123');
+
+    expect(nameElement).toBeInTheDocument();
+    expect(userElement).toBeInTheDocument();
+  });
+});
+
+describe('GenericModelPage Component', () => {
+  it('should render the component with the provided props', () => {
+    const sampleProps = {
+      model: 'Sample Model',
+      modelCard: () => <div>Mock Model Card</div>,
+      instances: [
+        { id: 1, name: 'Instance 1' },
+        { id: 2, name: 'Instance 2' },
+      ],
+    };
+
+    render(<GenericModelPage {...sampleProps} />);
+    
+    const modelHeader = screen.getByText('Sample Model');
+    const searchBar = screen.getByRole('textbox');
+
+    expect(modelHeader).toBeInTheDocument();
+    expect(searchBar).toBeInTheDocument();
+  });
+});
+
+jest.mock('../Components/CharityCard', () => {
+  return () => <div data-testid="charity-card">Mock Charity Card</div>;
+});
+
+describe('CharityModelPage Component', () => {
+  it('should render the CharityModelPage component with the provided data', () => {
+    render(<CharityModelPage />);
+    render(<CharityCard />);
+    const modelHeader = screen.getByText('Charities');
+
+    expect(modelHeader).toBeInTheDocument();
+  });
+});
+
+jest.mock('../Components/NewsCard', () => {
+  return () => <div data-testid="news-card">Mock News/Events Card</div>;
+});
+
+describe('NewsEventsModelPage Component', () => {
+  it('should render the NewsEventsModelPage component with the provided data', () => {
+    render(<NewsEventsModelPage />);
+    render(<NewsCard />);
+    const modelHeader = screen.getByText('News');
+
+    expect(modelHeader).toBeInTheDocument();
+  });
+});
+
+jest.mock('../Components/CountryCard', () => {
+  return () => <div data-testid="country-card">Mock Country Card</div>;
+});
+
+describe('CountryModelPage Component', () => {
+  it('should render the CountryModelPage component with the provided data', () => {
+    render(<CountryModelPage />);
+    render(<CountryCard />);
+    const modelHeader = screen.getByText('Countries');
+
+    expect(modelHeader).toBeInTheDocument();
+  });
+});
+
+describe('TeamCard Component', () => {
+  it('should render the team card with the correct information', () => {
+    const sampleData = {
+      name: 'Jack Test',
+      image: 'jack-test-image.jpg',
+      user: 'jacktest123',
+      role: 'Reporter',
+      bio: 'Loves technology',
+      commits: 100,
+      issues: 50,
+    };
+
+    render(<TeamCard {...sampleData} />);
+
+    const roleElement = screen.getByText('Reporter');
+    const bioElement = screen.getByText('Loves technology');
+
+    expect(roleElement).toBeInTheDocument();
+    expect(bioElement).toBeInTheDocument();
+  });
+});
+
+jest.mock('../Components/HomeCarousel', () => {
+  return () => <div data-testid="mocked-carousel">Mocked Carousel</div>;
+});
+
+describe('HomePage Component', () => {
+  it('should render the HomePage component', () => {
+    render(<HomePage />);
+
+    const carousel = screen.getByTestId('mocked-carousel');
+
+    expect(carousel).toBeInTheDocument();
   });
 });
