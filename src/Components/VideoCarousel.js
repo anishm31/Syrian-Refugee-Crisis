3bf7639639c0d044e059de1b0ce3465df2244d33
Matthew Albert <matthew.albert@utexas.edu>
2023-10-20 17:41:29 -0500

    responsive design for charity youtube videos

--- src/Components/VideoCarousel.js
+++ src/Components/VideoCarousel.js
@@ -1,5 +1,6 @@
 import YouTube from 'react-youtube';
 import Carousel from 'react-bootstrap/Carousel';
+import "../CSS/VideoCarousel.css"
 
 function VideoCarousel(props) {
   return (
@@ -7,8 +8,10 @@ function VideoCarousel(props) {
       {props.youtubeInfo.relevant_videos.map((video_id) => {
         console.log(video_id);
         return (
-          <Carousel.Item interval={6000}>
-            <YouTube videoId={video_id} />
+          <Carousel.Item style={{ display: 'flex', justifyContent: 'center'}} interval={6000}>
+            <div className='youtube-container'>
+              <YouTube videoId={video_id} />
+            </div>
           </Carousel.Item>
         );
       })}
