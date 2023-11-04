1507a3cceb5f9d03a77e5afb14ceb0bf1102c2b7
Anish Madgula <animadgula@utexas.edu>
2023-11-04 15:39:43 -0500

    added model searching on charity page

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-function SearchBar() {
+function SearchBar(props) {
+
+    const [query, setQuery] = useState(''); // Add state for the search query
+
+    const handleSearch = () => {
+      props.setSearchQuery(query); // Set the search query in the parent component
+      props.onSearch(); // Trigger the search function in the parent component
+    };
+
     return (
         <Container className='mt-4' style={{width : '35%'}}>
             <Form>
                 <InputGroup className='mt-5'>
-                    <Form.Control type='text' placeholder='Search for instance'/>
-                    <Button variant='outline-secondary' id='button-addon1' type='submit'>Search</Button>
+                    <Form.Control type='text' placeholder='Search for instance' value={query} onChange={(e) => setQuery(e.target.value)}/>
+                    <Button variant='outline-secondary' id='button-addon1' type='submit' onClick={handleSearch}>Search</Button>
                 </InputGroup>
             </Form>
         </Container>
--- src/Components/SearchResultsPage.js
+++ src/Components/SearchResultsPage.js
@@ -3,8 +3,16 @@ import CharityModelPage from "./CharityModelPage";
 import CountryModelPage from "./CountryModelPage"
 import NewsEventsModelPage from "./NewsEventsModelPage"
 import Container from "react-bootstrap/Container";
+// import { useLocation } from "react-router-dom";
 
 const SearchResultsPage = () => {
+    // // from ParkScape (https://gitlab.com/sarthaksirotiya/cs373-idb/)
+    // const location = useLocation();
+    // var search = location.pathname.split("/search/").at(-1);
+    // if (search.includes("%20")) {
+    //     search = search.replace("%20", " ");
+    // }
+    // search = search.replace("/", "");
     return (
         <>
         {/* <h1>Hello</h1> */}
