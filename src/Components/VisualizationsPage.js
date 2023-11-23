b6a9336f1bf094e6c8cc101a08790f0f7d55e21e
Anish Madgula <animadgula@utexas.edu>
2023-11-23 13:20:04 -0600

    working version of charities pie chart

--- src/Components/VisualizationsPage.js
+++ src/Components/VisualizationsPage.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import WorldMap from "./Visualizations/Our_Visualizations/Country/WorldMap";
+import { Doughnut } from 'react-chartjs-2';
+import axios from "axios";
+import Chart from 'chart.js/auto';
+
 
 function VisualizationsPage() {
   // State for visualization dimensions
   const [visDimensions, setVisDimensions] = useState({width: 0, height: 0});
+  const [charitiesData, setCharitiesData] = useState(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://api.syrianrefugeecrisis.me/charities")
+      .then((response) => {
+        setCharitiesData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   // useEffect for handling visualization dimensions and responsive resizing
   useEffect(() => {
@@ -22,9 +38,60 @@ function VisualizationsPage() {
     // Add event listener to update dimensions on window resize
     window.addEventListener("resize", updateDimensions);
 
-    // Cleanup function to remove event listener
-    return () => window.removeEventListener("resize", updateDimensions);
+    return () => {
+      window.removeEventListener("resize", updateDimensions);
+    }
   }, []);
+
+  useEffect(() => {
+    if (!charitiesData) return;
+
+    // Destroy the existing chart instance before creating a new one
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+
+    const newChartInstance = new Chart("dummyCanvasId", {
+      type: "doughnut", // or your desired chart type
+      data: generateReliefChartData(),
+      options: {
+        // your options here
+      },
+    });
+
+    // Set the new chart instance
+    setChartInstance(newChartInstance);
+
+    // Cleanup function to destroy the chart instance when the component is unmounted
+    return () => {
+      newChartInstance.destroy();
+    };
+  }, [charitiesData]); // Run whenever charitiesData changes
+
+  const generateReliefChartData = () => {
+    if (!charitiesData) return null;
+
+    const reliefCount = {};
+    charitiesData.data.forEach(charity => {
+      charity.relief_provided.forEach(relief => {
+        reliefCount[relief] = (reliefCount[relief] || 0) + 1;
+      });
+    });
+
+    const labels = Object.keys(reliefCount);
+    const data = Object.values(reliefCount);
+
+    return {
+      labels,
+      datasets: [
+        {
+          data,
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9800', '#9C27B0'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9800', '#9C27B0'],
+        }
+      ],
+    };
+  };
   
   return (
       <div style={{height: "100vh"}}>
@@ -32,6 +99,26 @@ function VisualizationsPage() {
           <br></br>
           <Container style={{height: "65%", width: "85%"}} fluid id="visContainer">
             <WorldMap height={visDimensions.height} width={visDimensions.width}/>
+            {charitiesData && (
+              <div>
+                <Doughnut
+                  data={generateReliefChartData()}
+                  options={{
+                    tooltips: {
+                      callbacks: {
+                        label: (tooltipItem, data) => {
+                          const dataset = data.datasets[tooltipItem.datasetIndex];
+                          const total = dataset.data.reduce((accumulator, currentValue) => accumulator + currentValue);
+                          const currentValue = dataset.data[tooltipItem.index];
+                          const percentage = ((currentValue / total) * 100).toFixed(2) + "%";
+                          return `${data.labels[tooltipItem.index]}: ${percentage}`;
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
+            )}
           </Container>
       </div>
   )
