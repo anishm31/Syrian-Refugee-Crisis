a79f3c95f95334769dc0a902e52dd414b2e9b009
Anish Madgula <animadgula@utexas.edu>
2023-11-23 18:37:51 -0600

    stable pie chart version with legend

--- src/Components/VisualizationsPage.js
+++ src/Components/VisualizationsPage.js
@@ -7,7 +7,6 @@ import Chart from 'chart.js/auto';
 
 
 function VisualizationsPage() {
-  // State for visualization dimensions
   const [visDimensions, setVisDimensions] = useState({width: 0, height: 0});
   const [charitiesData, setCharitiesData] = useState(null);
   const [chartInstance, setChartInstance] = useState(null);
@@ -28,20 +27,38 @@ function VisualizationsPage() {
       const containerWidth = document.getElementById("visContainer").offsetWidth;
       const containerHeight = document.getElementById("visContainer").offsetHeight;
 
-      // Set visualization dimensions to be 95% of parent container dimensions
       setVisDimensions({width: Math.floor(containerWidth * 0.95), height: Math.floor(containerHeight * 0.95)});
     }
 
-    // Setup dimensions on page load
+    // update dimensions when page loads
     updateDimensions();
-
-    // Add event listener to update dimensions on window resize
     window.addEventListener("resize", updateDimensions);
 
     return () => {
       window.removeEventListener("resize", updateDimensions);
     }
   }, []);
+
+  useEffect(() => {
+    if (!charitiesData) return;
+
+    // destroy existing chart instance before creating a new one
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+    const newChartInstance = new Chart("dummyCanvasId", {
+      type: "doughnut", // or your desired chart type
+      data: generateReliefChartData(),
+      options: {
+      },
+    });
+    setChartInstance(newChartInstance);
+    // destroy the chart instance when the component is unmounted
+    return () => {
+      newChartInstance.destroy();
+    };
+  }, []);
+
 
   const generateReliefChartData = () => {
     if (!charitiesData) return null;
@@ -56,46 +73,87 @@ function VisualizationsPage() {
     const labels = Object.keys(reliefCount);
     const data = Object.values(reliefCount);
 
+    const customColors = [
+      '#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9800', '#9C27B0',
+      '#2196F3', '#FF5722', '#607D8B', '#E91E63', '#795548', '#00BCD4',
+      '#8BC34A', '#673AB7', '#CDDC39', '#FF5252', '#FFC107', '#3F51B5',
+      '#009688', '#FFD600', '#FF4081', '#8D6E63', '#FF5722', '#536DFE'
+    ];
     return {
       labels,
       datasets: [
         {
           data,
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9800', '#9C27B0'],
-          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9800', '#9C27B0'],
+          backgroundColor: customColors.slice(0, data.length),
+          hoverBackgroundColor: customColors.slice(0, data.length),
         }
       ],
     };
   };
+
+  const generateLegend = () => {
+    const data = generateReliefChartData();
+    if (!data) return null;
+  
+    return (
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {data.labels.map((label, index) => (
+          <li key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
+            <span
+              style={{
+                display: 'inline-block',
+                width: '12px',
+                height: '12px',
+                backgroundColor: data.datasets[0].backgroundColor[index],
+                marginRight: '5px',
+              }}
+            />
+            <span style={{ fontSize: '12px' }}>{`${label}: ${data.datasets[0].data[index]} charities`}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  };
   
   return (
+    <div>
       <div style={{height: "100vh"}}>
           <h1 style={{textAlign : "center", marginBottom : "20px"}}>Our Visualizations</h1>
           <br></br>
           <Container style={{height: "65%", width: "85%"}} fluid id="visContainer">
             <WorldMap height={visDimensions.height} width={visDimensions.width}/>
-            {charitiesData && (
-              <div>
-                <Doughnut
-                  data={generateReliefChartData()}
-                  options={{
-                    tooltips: {
-                      callbacks: {
-                        label: (tooltipItem, data) => {
-                          const dataset = data.datasets[tooltipItem.datasetIndex];
-                          const total = dataset.data.reduce((accumulator, currentValue) => accumulator + currentValue);
-                          const currentValue = dataset.data[tooltipItem.index];
-                          const percentage = ((currentValue / total) * 100).toFixed(2) + "%";
-                          return `${data.labels[tooltipItem.index]}: ${percentage}`;
-                        },
-                      },
-                    },
-                  }}
-                />
-              </div>
-            )}
           </Container>
       </div>
+      <div>
+        <Container style={{ width: "60%", height: "60%" }}>
+          {charitiesData && (
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ flex: 1 }}>
+                  <Doughnut
+                    data={generateReliefChartData()}
+                    options={{
+                      tooltips: {
+                        callbacks: {
+                          label: (tooltipItem, data) => {
+                            const dataset = data.datasets[tooltipItem.datasetIndex];
+                            const total = dataset.data.reduce((accumulator, currentValue) => accumulator + currentValue);
+                            const currentValue = dataset.data[tooltipItem.index];
+                            const percentage = ((currentValue / total) * 100).toFixed(2) + "%";
+                            return `${data.labels[tooltipItem.index]}: ${percentage}`;
+                          },
+                        },
+                      },
+                    }}
+                  />
+                </div>
+                <div style={{ width: '30%', padding: '20px' }}>
+                  {generateLegend()}
+                </div>
+              </div>
+            )}
+        </Container>
+      </div>
+    </div>
   )
 }
 
