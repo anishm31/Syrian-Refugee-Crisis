b91aa799ecc8a81908ee39092bf630c3fd6d0104
Anish Madgula <animadgula@utexas.edu>
2023-11-27 14:08:32 -0600

    changed provider pie chart visualization data

--- src/Components/Visualizations/Provider_Visualizations/TreatmentPieChart.js
+++ src/Components/Visualizations/Provider_Visualizations/TreatmentPieChart.js
@@ -15,65 +15,79 @@ const TreatmentPieChart = () => {
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
-        setLoading(false); // Set loading to false once data is fetched
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  // Preprocess the data to aggregate counts for each treatment_type
+  // Preprocess the data to aggregate counts for each disorder
   const aggregatedData = treatmentData.reduce((acc, entry) => {
-    const existingEntry = acc.find((item) => item.treatment_type === entry.treatment_type);
+    entry.disorders_list.forEach(disorder => {
+      const existingEntry = acc.find(item => item.disorder === disorder);
 
-    if (existingEntry) {
-      existingEntry.count += 1;
-    } else {
-      acc.push({ treatment_type: entry.treatment_type, count: 1 });
-    }
+      if (existingEntry) {
+        existingEntry.count += 1;
+      } else {
+        acc.push({ disorder, count: 1 });
+      }
+    });
 
     return acc;
   }, []);
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF'];
+  const sortedData = aggregatedData.sort((a, b) => b.count - a.count);
+
+  // display only the top N disorders
+  const topN = 10;
+  const topNData = sortedData.slice(0, topN);
+  const otherData = sortedData.slice(topN);
+
+  // Aggregate the counts of the "Other" category
+  const otherCount = otherData.reduce((acc, entry) => acc + entry.count, 0);
+  const finalData = [...topNData, { disorder: 'Other', count: otherCount }];
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#FF6666'];
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Spinner animation="border" variant="primary" role="status" style={{ display: loading ? 'block' : 'none' }} />
-      {!loading && (
-        <div>
-          <h3 style={{ textAlign: 'center' }}>Mental Health Treatments</h3>
-          {aggregatedData.length > 0 && (
-            <PieChart width={800} height={600}>
-              <Pie
-                data={aggregatedData}
-                cx={400}
-                cy={300}
-                outerRadius={200}
-                fill="#8884d8"
-                dataKey="count"
-                label
-              >
-                {aggregatedData.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-              <Tooltip formatter={(value, name, props) => [value, props.payload.treatment_type]} />
-              <Legend
-                verticalAlign="middle"
-                align="right"
-                layout="vertical"
-                payload={aggregatedData.map((entry, index) => ({
-                  value: entry.treatment_type,
-                  type: 'square',
-                  color: COLORS[index % COLORS.length],
-                }))}
-              />
-            </PieChart>
-          )}
-        </div>
-      )}
-    </div>
+    <Spinner animation="border" variant="primary" role="status" style={{ display: loading ? 'block' : 'none' }} />
+    {!loading && (
+      <div>
+        <h3 style={{ textAlign: 'center' }}>Mental Health Treatments</h3>
+        {finalData.length > 0 && (
+          <PieChart width={800} height={600}>
+            <Pie
+              data={finalData}
+              cx={300}
+              cy={300}
+              outerRadius={200}
+              fill="#8884d8"
+              dataKey="count"
+              label
+            >
+              {finalData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip formatter={(value, name, props) => [value, props.payload.disorder]} />
+            <Legend
+              verticalAlign="middle"
+              align="right"
+              layout="vertical"
+              wrapperStyle={{ right: '-120px', top: '30%' }}
+              payload={finalData.map((entry, index) => ({
+                value: entry.disorder,
+                type: 'square',
+                color: COLORS[index % COLORS.length],
+              }))}
+            />
+          </PieChart>
+        )}
+      </div>
+    )}
+  </div>
   );
 };
 
