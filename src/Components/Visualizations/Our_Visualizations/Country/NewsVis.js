1e114631e921d2b123bd308de9e96e301bca6af0
Shadow Sincross <shadowanneruby@gmail.com>
2023-11-29 10:50:24 -0600

    tooltip added

--- src/Components/Visualizations/Our_Visualizations/Country/NewsVis.js
+++ src/Components/Visualizations/Our_Visualizations/Country/NewsVis.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
+import { Tooltip } from "@visx/tooltip";
+import { useTooltip, defaultStyles } from "@visx/tooltip";
+import { localPoint } from "@visx/event";
 
 const NewsVis = ({ newsData }) => {
   const ref = useRef();
   const [dataset, setDataset] = useState([]);
+  const { tooltipData, tooltipLeft, tooltipTop, showTooltip, hideTooltip } = useTooltip();
 
   useEffect(() => {
-    // Generate dataset based on the number of sources
     const allShortnames = newsData.data.flatMap((d) =>
       (d.sources || []).map((source) => source.source_short_name)
     );
-    const numberOfSources = allShortnames.length;
 
     const sourceCounts = d3.rollup(
       allShortnames,
@@ -20,21 +22,21 @@ const NewsVis = ({ newsData }) => {
 
     const sourceData = Array.from(sourceCounts, ([shortname, count]) => ({
       shortname,
-      radius: Math.sqrt(count) * 5, // Adjust the scaling factor as needed
+      radius: Math.sqrt(count) * 5,
     }));
 
     const newDataset = sourceData.map((data) => ({
-      cx: Math.random() * 500 + 10, // Adjust the range for x positions
-      cy: Math.random() * 500 + 15, // Centered around the middle of the available height
-      r: data.radius, // Use the calculated radius from sourceData
+      cx: Math.random() * 500 + 10,
+      cy: Math.random() * 500 + 15,
+      r: data.radius,
+      name: data.shortname,
     }));
 
-    // Ensure non-overlapping positions
     setDataset(enforceNonOverlapping(newDataset));
   }, [newsData]);
 
   const enforceNonOverlapping = (newDataset) => {
-    const margin = 5; // Adjust the margin to control the spacing between circles
+    const margin = 5;
     const maxAttempts = 1000;
 
     return newDataset.map((circle) => {
@@ -47,8 +49,8 @@ const NewsVis = ({ newsData }) => {
               other.r + circle.r + margin
         )
       ) {
-        circle.cx = Math.random() * 500 + 10; // Adjust the range for x positions
-        circle.cy = Math.random() * 500 + 15; // Centered around the middle of the available height
+        circle.cx = Math.random() * 500 + 10;
+        circle.cy = Math.random() * 500 + 15;
 
         attempts += 1;
         if (attempts > maxAttempts) {
@@ -66,29 +68,72 @@ const NewsVis = ({ newsData }) => {
 
     const svgElement = d3.select(ref.current);
 
-    // Select all existing circles and bind data
     const circles = svgElement.selectAll("circle").data(dataset);
 
-    // Update existing circles
     circles
       .attr("cx", (d) => d.cx)
       .attr("cy", (d) => d.cy)
-      .attr("r", (d) => d.r);
+      .attr("r", (d) => d.r)
+      .style("fill", (d) => getColorBasedOnRadius(d.r))
+      .on("mouseover", (event, d) => handleMouseOver(event, d))
+      .on("mouseout", handleMouseOut);
 
-    // Enter new circles
     circles
       .enter()
       .append("circle")
       .attr("cx", (d) => d.cx)
       .attr("cy", (d) => d.cy)
       .attr("r", (d) => d.r)
-      .style("fill", "blue");
+      .style("fill", (d) => getColorBasedOnRadius(d.r))
+      .on("mouseover", (event, d) => handleMouseOver(event, d))
+      .on("mouseout", handleMouseOut);
 
-    // Exit removed circles
     circles.exit().remove();
   }, [dataset]);
 
-  return <svg width={1000} height={1000} ref={ref} />;
+  const handleMouseOver = (event, data) => {
+    const { x, y } = localPoint(event);
+    showTooltip({
+      tooltipLeft: x,
+      tooltipTop: y,
+      tooltipData: data,
+    });
+  };
+
+  const handleMouseOut = () => {
+    hideTooltip();
+  };
+
+  const getColorBasedOnRadius = (radius) => {
+    return d3.interpolateRdYlBu(radius / 30);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <svg width={1000} height={1000} ref={ref}>
+        {dataset.map((d, i) => (
+          <circle
+            key={i}
+            cx={d.cx}
+            cy={d.cy}
+            r={d.r}
+            fill={getColorBasedOnRadius(d.r)}
+            onMouseOver={(event) => handleMouseOver(event, d)}
+            onMouseOut={handleMouseOut}
+          />
+        ))}
+      </svg>
+      {tooltipData && (
+        <Tooltip
+          top={tooltipTop}
+          left={tooltipLeft}
+          style={defaultStyles}
+        >
+          <strong>{tooltipData.name}</strong>
+        </Tooltip>
+      )}
+    </div>
+  );
 };
 
 export default NewsVis;
