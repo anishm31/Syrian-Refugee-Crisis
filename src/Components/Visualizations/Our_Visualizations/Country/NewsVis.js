4958f5693f8de2701454956b5fdaafddc06a2a28
Shadow Sincross <shadowanneruby@gmail.com>
2023-11-28 23:29:41 -0600

    got the circles appearing

---
+++ src/Components/Visualizations/Our_Visualizations/Country/NewsVis.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const NewsVis = ({ newsData }) => {
+  const ref = useRef();
+  const [dataset, setDataset] = useState([]);
+
+  useEffect(() => {
+    // Generate dataset based on the number of sources
+    const allShortnames = newsData.data.flatMap((d) =>
+      (d.sources || []).map((source) => source.source_short_name)
+    );
+    const numberOfSources = allShortnames.length;
+
+    const newDataset = Array(numberOfSources).fill(0).map(() => ({
+      cx: Math.random() * 500 + 10, // Adjust the range for x positions
+      cy: Math.random() * 500 + 15, // Centered around the middle of the available height
+      r: Math.random() * 1 + 5, // Adjust the radius as needed
+    }));
+
+    // Ensure non-overlapping positions
+    setDataset(enforceNonOverlapping(newDataset));
+  }, [newsData]);
+
+  const enforceNonOverlapping = (newDataset) => {
+    const margin = 5; // Adjust the margin to control the spacing between circles
+    const maxAttempts = 1000;
+
+    return newDataset.map((circle) => {
+      let attempts = 0;
+      while (
+        newDataset.some(
+          (other) =>
+            other !== circle &&
+            Math.hypot(other.cx - circle.cx, other.cy - circle.cy) <
+              other.r + circle.r + margin
+        )
+      ) {
+        circle.cx = Math.random() * 500 + 10; // Adjust the range for x positions
+        circle.cy = Math.random() * 500 + 15; // Centered around the middle of the available height
+
+        attempts += 1;
+        if (attempts > maxAttempts) {
+          console.warn("Max attempts reached. Some circles may still overlap.");
+          break;
+        }
+      }
+
+      return circle;
+    });
+  };
+
+  useEffect(() => {
+    if (dataset.length === 0) return;
+
+    const svgElement = d3.select(ref.current);
+
+    // Select all existing circles and bind data
+    const circles = svgElement.selectAll("circle").data(dataset);
+
+    // Update existing circles
+    circles
+      .attr("cx", (d) => d.cx)
+      .attr("cy", (d) => d.cy)
+      .attr("r", (d) => d.r);
+
+    // Enter new circles
+    circles
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => d.cx)
+      .attr("cy", (d) => d.cy)
+      .attr("r", (d) => d.r)
+      .style("fill", "blue");
+
+    // Exit removed circles
+    circles.exit().remove();
+  }, [dataset]);
+
+  return <svg width={1000} height={1000} ref={ref} />;
+};
+
+export default NewsVis;
--- src/Components/VisualizationsPage.js
+++ src/Components/VisualizationsPage.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import WorldMap from "./Visualizations/Our_Visualizations/Country/WorldMap";
 import CharityPieChart from "./Visualizations/Our_Visualizations/Country/CharityPieChart";
 import NewsBarGraph from "./Visualizations/Our_Visualizations/Country/NewsBarGraph";
+import NewsVis from "./Visualizations/Our_Visualizations/Country/NewsVis";
 import axios from "axios";
 
 
@@ -55,17 +56,17 @@ function VisualizationsPage() {
           <h1 style={{textAlign : "center", marginBottom : "20px"}}>Our Visualizations</h1>
           <br></br>
           <Container style={{height: "65%", width: "85%"}} fluid id="visContainer">
-            <WorldMap height={visDimensions.height} width={visDimensions.width}/>
+            {/* <WorldMap height={visDimensions.height} width={visDimensions.width}/> */}
           </Container>
       </div>
       <div>
         <Container style={{ width: "60%", height: "60%" , marginBottom: "20px"}}>
-          {charitiesData && <CharityPieChart charitiesData={charitiesData} />}
+          {/* {charitiesData && <CharityPieChart charitiesData={charitiesData} />} */}
         </Container>
       </div>
       <div>
       <Container style={{ width: "50%", height: "50%" }}>
-      {newsData && <NewsBarGraph newsData={newsData} />}
+      {newsData && <NewsVis newsData={newsData} />}
         </Container>
       </div>
     </div>
