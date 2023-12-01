fa463e023773ce8d495e4f53304d6ff530b4b38b
Matthew Albert <matthew.albert@utexas.edu>
2023-11-30 19:41:36 -0600

    bubble graph bug fix

--- src/Components/Visualizations/Our_Visualizations/Country/NewsVis.js
+++ src/Components/Visualizations/Our_Visualizations/Country/NewsVis.js
@@ -3,8 +3,10 @@ import * as d3 from "d3";
 import { Tooltip } from "@visx/tooltip";
 import { useTooltip, defaultStyles } from "@visx/tooltip";
 import { localPoint } from "@visx/event";
+import { Spinner } from "react-bootstrap";
+import axios from "axios";
 
-const NewsVis = ({ newsData }) => {
+const NewsVis = () => {
   const ref = useRef();
   const [dataset, setDataset] = useState([]);
   const { tooltipData, tooltipLeft, tooltipTop, showTooltip, hideTooltip } = useTooltip();
@@ -16,32 +18,40 @@ const NewsVis = ({ newsData }) => {
     .range(Array.from({ length: thresholds.length + 1 }, (_, i) => colorScheme(i / thresholds.length)));
 
   useEffect(() => {
-    const allShortnames = newsData.data.flatMap((d) =>
-      (d.sources || []).map((source) => source.source_short_name)
-    );
+    axios.get("https://api.syrianrefugeecrisis.me/news-and-events")
+    .then((response) => {
+      const newsData = response.data;
+      console.log(newsData)
+        const allShortnames = newsData.data.flatMap((d) =>
+        (d.sources || []).map((source) => source.source_short_name)
+      );
 
-    const sourceCounts = d3.rollup(
-      allShortnames,
-      (v) => v.length,
-      (d) => d || "Unknown"
-    );
+      const sourceCounts = d3.rollup(
+        allShortnames,
+        (v) => v.length,
+        (d) => d || "Unknown"
+      );
 
-    const sourceData = Array.from(sourceCounts, ([shortname, count]) => ({
-      shortname,
-      radius: Math.sqrt(count) * 5,
-      count,
-    }));
+      const sourceData = Array.from(sourceCounts, ([shortname, count]) => ({
+        shortname,
+        radius: Math.sqrt(count) * 5,
+        count,
+      }));
 
-    const newDataset = sourceData.map((data) => ({
-      cx: Math.random() * 500 + 10,
-      cy: Math.random() * 500 + 15,
-      r: data.radius,
-      name: data.shortname,
-      count: data.count,
-    }));
+      const newDataset = sourceData.map((data) => ({
+        cx: Math.random() * 500 + 10,
+        cy: Math.random() * 500 + 15,
+        r: data.radius,
+        name: data.shortname,
+        count: data.count,
+      }));
 
-    setDataset(enforceNonOverlapping(newDataset));
-  }, [newsData]);
+      setDataset(enforceNonOverlapping(newDataset));
+      })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   const enforceNonOverlapping = (newDataset) => {
     const margin = 5;
@@ -70,34 +80,6 @@ const NewsVis = ({ newsData }) => {
       return circle;
     });
   };
-
-  useEffect(() => {
-    if (dataset.length === 0) return;
-
-    const svgElement = d3.select(ref.current);
-
-    const circles = svgElement.selectAll("circle").data(dataset);
-
-    circles
-      .attr("cx", (d) => d.cx)
-      .attr("cy", (d) => d.cy)
-      .attr("r", (d) => d.r)
-      .style("fill", (d) => getColorBasedOnRadius(d.count))
-      .on("mouseover", (event, d) => handleMouseOver(event, d))
-      .on("mouseout", handleMouseOut);
-
-    circles
-      .enter()
-      .append("circle")
-      .attr("cx", (d) => d.cx)
-      .attr("cy", (d) => d.cy)
-      .attr("r", (d) => d.r)
-      .style("fill", (d) => getColorBasedOnRadius(d.count))
-      .on("mouseover", (event, d) => handleMouseOver(event, d))
-      .on("mouseout", handleMouseOut);
-
-    circles.exit().remove();
-  }, [dataset]);
 
   const handleMouseOver = (event, data) => {
     const { x, y } = localPoint(event);
@@ -152,7 +134,10 @@ const NewsVis = ({ newsData }) => {
   };
 
   return (
+    dataset.length > 0
+    ?
     <div style={{ position: "relative" }}>
+      <h3 style={{textAlign : "center", marginBottom : "10px"}}>Frequency of Source Citations</h3>
       <svg width={600} height={600} ref={ref}>
         {dataset.map((d, i) => (
           <circle
@@ -166,7 +151,7 @@ const NewsVis = ({ newsData }) => {
           />
         ))}
         <g transform={`translate(0, ${600 - 80})`}>
-          {createMapLegend(10, 200)}
+          {createMapLegend(10, 300)}
         </g>
       </svg>
       {tooltipData && (
@@ -181,6 +166,10 @@ const NewsVis = ({ newsData }) => {
           <div>Count: {tooltipData.count}</div>
         </Tooltip>
       )}
+    </div>
+    :
+    <div style={{paddingTop: "20px", textAlign : "center"}}>
+      <Spinner animation="border" variant="primary" />
     </div>
   );
 };
